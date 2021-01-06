3c119fa0fd59d57fac7d1b4721ef171021d9c35a
USER <USER@DESKTOP-U9S252A>
2021-01-06 15:20:37 +0900

    first

---
+++ lecture/src/com/urdomain/ch01/Hello.java
@@ -0,0 +1,16 @@
+package com.urdomain.ch01;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, welcome to the java World!");
+	}
+		
+//	public double aa(int bbb) {
+//		System.out.println("###" + bbb);
+//		double a = 1;
+//		a = a + bbb;
+//		
+//		a = bbb;
+//		return a; 
+//	}
+}
